decefcfe847451e8e7fc1d9cef9c7871d85979e8
chieger <charlie@codepath.com>
2017-06-17 15:42:51 -0700

    setup collection view

---
+++ flix_demo_03/PosterCell.swift
@@ -0,0 +1,15 @@
+//
+//  PosterCell.swift
+//  flix_demo_03
+//
+//  Created by Charles Hieger on 6/17/17.
+//  Copyright © 2017 Charles Hieger. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+   @IBOutlet weak var posterImageView: UIImageView!
+
+}
